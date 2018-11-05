90ca7d742c2758615dd109004d56f931037b97b1
Xwaffle <chasecool56@gmail.com>
2018-11-05 18:39:22 -0500

    Started writing basic database infrastructure in .sql file.

---
+++ AllTables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Artists (
+    artistID INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+CREATE TABLE Albums (
+    albumID INT PRIMARY KEY,
+    artistID INT,
+    name VARCHAR(50),
+	year INT,
+    FOREIGN KEY(artistID) REFERENCES 	Artists(artistID));
+);
+CREATE TABLE Songs (
+    songID INT PRIMARY KEY,
+    albumID INT,
+	trackNum INT,
+    name VARCHAR(50),
+    FOREIGN KEY(albumID) REFERENCES 	Albums(albumID));
+);
